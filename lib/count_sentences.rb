07ad9c2b67456eaa169f441e9da938f45df9221b
lucassherwin <lksherwin@gmail.com>
2020-02-22 18:35:10 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
 
 class String
-
+  # attr_accessor :sentence
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #binding.pry
+    self.split(/\.|\?|\!/).delete_if {|i| i.size < 1}.size
   end
 end
