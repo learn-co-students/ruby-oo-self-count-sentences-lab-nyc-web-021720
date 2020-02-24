a3b0055b6151c2fe489e57c0b5e1df046b66cf04
matthewpeak <matthewpea@gmail.com>
2020-02-23 20:46:21 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+    return self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+  end 
+  
 end
